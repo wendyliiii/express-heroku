630997bdbd8bcafe55e3ccb98b55ff21bc65d051
ggm <godgunman@gmail.com>
2017-08-13 23:35:40 +0800

    Add port env

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
 })
