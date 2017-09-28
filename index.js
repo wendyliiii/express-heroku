49ce36ddf9b08b48fb368da7bca7fd1ed4289d5c
wendyliiii <32361206+wendyliiii@users.noreply.github.com>
2017-09-28 08:41:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   res.send('
-  {"name": "李涵寧",
+  "name": "李涵寧",
   "email": "hanning@raphael.clinic"
-}')
+')
 })
 
 app.listen(port, function () {
