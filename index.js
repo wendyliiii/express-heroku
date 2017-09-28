d979d15873d4e8bc6787deb4426ca6001844b873
wendyliiii <32361206+wendyliiii@users.noreply.github.com>
2017-09-28 23:58:13 +0800

    final

--- index.js
+++ index.js
@@ -2,26 +2,25 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.get('/homework', function(req, res) {
-//     res.json({ "name": "李涵寧",
-//    "email": "hanning@raphael.clinic" });
-// });
-// app.get('/', function(req, res) {
-//    res.send('index!');
-// });
-
+app.get('/homework', function(req, res) {
+    res.json({ "name": "李涵寧",
+   "email": "hanning@raphael.clinic" });
+});
 app.get('/', function(req, res) {
-  res.send('home page!');
-});
-var router =  = express.Router();
-
-router.get('/homework', function(req, res) {
-   res.json({ "name": "李涵寧",
-  "email": "hanning@raphael.clinic" });
+   res.send('index!');
 });
 
-//app.use('/homework', router);
-app.use('/', router);
+// Express 4 方法
+// app.get('/', function(req, res) {
+//   res.send('home page!');
+// });
+// var router =  = express.Router();
+
+// router.get('/homework', function(req, res) {
+//    res.json({ "name": "李涵寧",
+//   "email": "hanning@raphael.clinic" });
+// });
+// app.use('/', router);
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
