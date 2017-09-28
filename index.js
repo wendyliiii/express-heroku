70a4d615ec031b8f1e7ad59c9ddb964c0e279a00
wendyliiii <32361206+wendyliiii@users.noreply.github.com>
2017-09-28 23:44:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const router =  = express.Router();
 
 const port = process.env.PORT || 3000
 
-app.get('/homework', function(req, res) {
-    res.json({ "name": "李涵寧",
-  "email": "hanning@raphael.clinic" });
+// app.get('/homework', function(req, res) {
+//     res.json({ "name": "李涵寧",
+//   "email": "hanning@raphael.clinic" });
  
+// });
+router.get('/', function(req, res) {
+  res.send('home page!');
+});
+
+router.get('/homework', function(req, res) {
+   res.json({ "name": "李涵寧",
+  "email": "hanning@raphael.clinic" });
 });
 
 //app.use('/homework', router);
+app.use('/', router);
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
