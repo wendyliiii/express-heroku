2f42effc83a47d2ecce4e4d1c2f81478d4c5b573
wendyliiii <32361206+wendyliiii@users.noreply.github.com>
2017-09-28 23:24:59 +0800

    modify index js code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const express = require('express')
+const app = express()
+const router =  = express.Router();
 
+const port = process.env.PORT || 3000
+
+router.get('/homework', function(req, res) {
+    res.json({ "name": "李涵寧",
+  "email": "hanning@raphael.clinic" });
+});
+
+
+app.use('/', router);
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
+})
