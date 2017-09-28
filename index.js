472ef35195776b2e1fea83360c662d9d727fd2a5
wendyliiii <32361206+wendyliiii@users.noreply.github.com>
2017-09-28 09:48:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
-  res.send('/homework')
+  res.send('homework')
 })
 
 app.listen(port, function () {
